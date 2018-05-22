5ad982c9ce6ec674c30714bd6a55c518bfeab6fd
Bhargava Pejakala Kakrannaya <BKakrannaya@walmartlabs.com>
2018-05-22 12:21:00 -0700

    HTML

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Welcome to BK Life')
+    res.render('index.html')
 })
 
 app.listen(app.get('port'), function() {
